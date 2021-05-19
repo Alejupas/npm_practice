466225aafe0a3ab317f35fb555e7bccd006d9cb1
Mindaugas Kapickas <mindaugas@mkp.com>
2021-05-19 09:46:46 +0300

    app js update

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const { add } = require("./functions");
 const _ = require("lodash");
-console.log("app.js");
+const { cloneDeep } = require("lodash");
 
+// console.log("app.js");
 // console.log(add(10, 10));
 
 //pradeti organizuoti savo koda su NPM
@@ -19,3 +20,23 @@ console.log("app.js");
 // su lodash random gauti skaiciu nuo 10 iki 30
 const randomNumber = _.random(10, 30);
 console.log(randomNumber);
+
+//clone lodash funkcija (shallow)
+// cloneDeep pakopijuos ir objekta
+//padaryti deep a kintamojo kopija  su _.cloneDeep
+
+const a = [{ age: 11 }, { age: 22 }, { age: 33 }];
+const maybeCopy = _.cloneDeep(a);
+console.log(maybeCopy);
+
+// const maybeCopy = a;
+
+maybeCopy[0].age = 100;
+
+console.log("a[0].age", a[0].age);
+
+if (maybeCopy[1] === a[1]) {
+  console.log("lygu");
+} else {
+  console.log("nelygu");
+}
